02eae840958ed619ea973f2654b1b57de1a50aeb
Francisco Faner <84994055+dev-seh@users.noreply.github.com>
2023-05-04 15:45:02 -0300

    Walk animation - Camera movement
    Added "Stardew Valley-ish" animation for player movement where the sprite flips on the X axis rather than having a separate animaton for each direction.
    
    Added cinecam to follow the player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,13 +9,17 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -31,8 +35,22 @@ public class PlayerController : MonoBehaviour
             if(count == 0){
                 rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
             }
+            
+            animator.SetBool("isMoving", true);
 
         }
+        else{
+            animator.SetBool("isMoving", false);
+        }
+
+        //set direction of sprite to movement direction
+        if(movementInput.x < 0){
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0){
+            spriteRenderer.flipX = false;
+        }
+        
     }
 
     void OnMove(InputValue movementValue){
